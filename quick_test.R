5c780857f20bc9bebfcece8fb96e1c3a207e74f6
Matt Higham <mhigham@stlawu.edu>
2024-09-05 10:21:55 -0400

    tasks for September 12.

--- quick_test.R
+++ quick_test.R
@@ -15,10 +15,16 @@ data(tropicalsound)
 acoustic_complexity(tropicalsound)
 
 acoustic_complexity(tropicalsound, max_freq = 8000)
-
+library(tuneR)
 
 ## should put .wav files in a /data folder
 soundfile <- readWave(here::here("Eclipse-data-for-Matt/20240401_061600.wav"))
 
 soundfile.aci <- acoustic_complexity(soundfile)
-result <- ndsi(soundfile)
+summary(soundfile.aci)
+result <- ndsi(soundfile) ## not this one
+
+## guessing that right is missing because left is just the total
+## all left is amount of biophyny in the entire 50 second clip
+## by min divides by (50/60)
+## aci_fl_left_vals is amount of biophyny in a tiny interval within the clip
--- task_list.md
+++ task_list.md
@@ -6,3 +6,10 @@
 
 2. Familiarize yourself with soundecology package.
 
+## For September 12
+
+1. Create a line plot of ACI for one .wav file.
+
+2. Create a multiple line plots of ACI for a few .wav files.
+
+3. Investigate some of the other options besides ACI to graph from the .wav file.
