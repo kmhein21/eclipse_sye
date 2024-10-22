281dddb610dc930299f15eb289828642cd565517
kmhein21 <kmhein21@stlawu.edu>
2024-10-22 10:56:01 -0400

    Working on function

--- Function_RDA.R
+++ Function_RDA.R
@@ -1,12 +1,12 @@
-test_fun<- function(folder) {
+test_fun<- function(folder, output) {
   f_list<- list.files(folder, pattern = "\\.WAV$", full.names = TRUE)
   paths_date <-str_remove(f_list, paste0(here(), "/test_wavs"))
   
   WAV_f<-map(f_list, readWave)
   AEI<-map(WAV_f, acoustic_evenness)
   BEI<-map(WAV_f, bioacoustic_index)
-  #ADI<-map(WAV_f, acoustic_diversity)
-  #ACI<-map(WAV_f, acoustic_complexity)
+  ADI<-map(WAV_f, acoustic_diversity)
+  ACI<-map(WAV_f, acoustic_complexity)
   ndsi<-map(WAV_f, soundecology::ndsi)
   
   AEI_df<-as.data.frame(AEI)|>
@@ -25,7 +25,32 @@ test_fun<- function(folder) {
     select("biophony")
   
   single<- as.tibble(cbind(paths_date, AEI_df, BEI_df, Biophony_df))
-  print(single)
+  
+  ACI_all<-vector("list", length(c(1:3)))
+  ADI_all<-vector("list", length(c(1:3)))
+  n<-3
+  for (i in 1:n){
+    ACI_all[i]<-(as.data.frame(ACI[[i]]$aci_fl_left_vals))
+    ADI_all[i]<-(as.data.frame(ADI[[i]]$left_band_values))
+  } 
+  multiple<-as.tibble(cbind( ACI_all, ADI_all))
+  full<-bind_cols(single, multiple)
+  
+  final<- full|> mutate(biophony = as.numeric(biophony),
+                        aei = as.numeric(aei),
+                        bei = as.numeric(bei))|>
+    separate(paths_date, into = c("date","time_hms"), sep = "_")|>
+    separate(time_hms, into = c("time", "wav"), sep = "\\.")|>
+    separate(time, into = c("hours", "other"), sep = 2)|>
+    separate(other, into = c("min", "sec"), sep = 2)|>
+    mutate(date = parse_number(date))|>
+    unite("time", c("date", "hours", "min", "sec"), sep = ":")|>
+    mutate(time= ymd_hms(time))|>
+    select(-wav)
+
+    
+  save(final, file = output)
+  load(output)
 } 
 
-test_fun(here("test_wavs"))
+test_fun(here("test_wavs"), output = "test_3_wav.rda")
