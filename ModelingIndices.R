07fbed2e849acde3b9a1a31b49f84a311553cb47
kmhein21 <kmhein21@stlawu.edu>
2025-03-08 16:13:57 -0500

    Adding time tick marks

--- ModelingIndices.R
+++ ModelingIndices.R
@@ -30,6 +30,7 @@ modelEclipse_df<-fullAudio|> mutate(eclipse = ifelse(day == "2024-04-08",
   filter(hour>= eclipse_start & hour<= eclipse_end &
            day %in% c("2024-04-06", "2024-04-07", "2024-04-08",
                     "2024-04-09", "2024-04-11"))|>
+  mutate(day_factor = as.factor(day))|>
   select(hour, hour_numeric, everything())
 
 modelBeforeEclipse_df<- fullAudio|> mutate(eclipse = ifelse(day == "2024-04-08", 
@@ -38,7 +39,8 @@ modelBeforeEclipse_df<- fullAudio|> mutate(eclipse = ifelse(day == "2024-04-08",
   mutate(eclipse = as_factor(eclipse))|>
   mutate(hour_numeric = as.numeric(hour))|>
   filter(hour < eclipse_start & day %in% c("2024-04-06", "2024-04-07", "2024-04-08",
-                   "2024-04-09", "2024-04-11")) 
+                   "2024-04-09", "2024-04-11")) |>
+  mutate(day_factor = as.factor(day))
 
 # Creating a similar model to mh_exploration 
 # Spline on Time, Least squares for 'eclipse or not', and interaction between the two 
@@ -48,106 +50,86 @@ bei_mod<- mgcv::gam(bei~ s(hour_numeric, by = eclipse)+
                       eclipse,
                      data = modelEclipse_df)
 
-bei_mod<- mgcv::gam(bei~ s(hour_numeric, by = eclipse)+
-                      eclipse+
-                      folder_name, 
+# by day 
+
+bei_mod<- mgcv::gam(bei~ s(hour_numeric, by = day_factor)+
+                      day_factor,
                     data = modelEclipse_df)
-# adding the folder_name increases the deviance explained and makes the residuals look better
+
 
 summary(bei_mod)
 
 gam.check(bei_mod)
 
-grid <- modelEclipse_df |> ungroup() |>
+grid <- modelEclipse_df |> 
+  ungroup() |>
   data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-    eclipse = modelEclipse_df |> pull(eclipse) |> levels()
+    day_factor = modelEclipse_df |> pull(day_factor) |> levels()
   )
 
 gam_aug <- broom::augment(bei_mod, newdata = grid)
 
 
+
 # visual across the time of eclipse
 ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelEclipse_df, aes(y = bei, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
+  geom_point(data = modelEclipse_df, aes(y = bei, colour = day_factor), alpha = 0.1) +
+  geom_line(aes(colour = day_factor),
             linewidth = 2) +
   scale_colour_viridis_d() +
   theme_minimal() +
   labs(colour = "eclipse_or_not",
-       y = "bei")
+       y = "bei")+
+  scale_x_continuous(name = "Time", breaks = c(eclipse_start, hms(00,00,15), hms(00,00,16), eclipse_end))
 
-# x-axis: find some way to denote when totality is? 
+# x-axis: find some way to denote when totality is? Moon picture 
 
 # model and visual including time before eclipse
 
-bei_mod2<- mgcv::gam(bei~ s(hour_numeric, by = eclipse)+
-                      eclipse
+bei_mod2<- mgcv::gam(bei~ s(hour_numeric, by = day_factor)+
+                      day_factor
                     , data = modelBeforeEclipse_df)
 summary(bei_mod2)
 
 grid <- modelBeforeEclipse_df |> ungroup() |>
   data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-            eclipse = modelBeforeEclipse_df |> pull(eclipse) |> levels()
+            day_factor = modelEclipse_df |> pull(day_factor) |> levels()
   )
 
 gam_aug <- broom::augment(bei_mod2, newdata = grid)
 
 ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelBeforeEclipse_df, aes(y = bei, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
+  geom_point(data = modelBeforeEclipse_df, aes(y = bei, colour = day_factor), alpha = 0.1) +
+  geom_line(aes(colour = day_factor),
             linewidth = 2) +
   scale_colour_viridis_d() +
   theme_minimal() +
   labs(colour = "eclipse_or_not",
-       y = "bei")
+       y = "bei")+
+  scale_x_continuous(name = "Time", breaks = c(hms(00,45,5), hms(00,15,6), hms(00,45,6),hms(00,15,7)))
 
 
-
-
-
-
+# custom point shapes 
 
 
 ## Practice making other models 
 # Potential model for AEI 
 
-aei_mod<- mgcv::gam(aei~ s(hour_numeric, by = eclipse)+
-                      eclipse 
+aei_mod<- mgcv::gam(aei~ s(hour_numeric, by = day_factor)+
+                      day_factor 
                     , data = modelEclipse_df)
 summary(aei_mod)
 
 grid <- modelEclipse_df |> ungroup() |>
   data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-            eclipse = modelEclipse_df |> pull(eclipse) |> levels()
+            day_factor = modelEclipse_df |> pull(day_factor) |> levels()
   )
 
 gam_aug <- broom::augment(aei_mod, newdata = grid)
 
 ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelEclipse_df, aes(y = aei, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
-            linewidth = 2) +
-  scale_colour_viridis_d() +
-  theme_minimal() +
-  labs(colour = "eclipse_or_not",
-       y = "aei")
-
-# Looking at time before
-aei_mod2<- mgcv::gam(aei~ s(hour_numeric, by = eclipse)+
-                       eclipse
-                     , data = modelBeforeEclipse_df)
-summary(aei_mod2)
-
-grid <- modelBeforeEclipse_df |> ungroup() |>
-  data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-            eclipse = modelBeforeEclipse_df |> pull(eclipse) |> levels()
-  )
-
-gam_aug <- broom::augment(aei_mod2, newdata = grid)
-
-ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelBeforeEclipse_df, aes(y = aei, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
+  geom_point(data = modelEclipse_df, aes(y = aei, colour = day_factor), alpha = 0.1) +
+  geom_line(aes(colour = day_factor),
             linewidth = 2) +
   scale_colour_viridis_d() +
   theme_minimal() +
@@ -156,49 +138,27 @@ ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
 
 # Potential model for ADI 
 
-ADI_mod<- mgcv::gam(fullADI~ s(hour_numeric, by = eclipse)+
-                      eclipse 
+ADI_mod<- mgcv::gam(fullADI~ s(hour_numeric, by = day_factor)+
+                      day_factor 
                     , data = modelEclipse_df)
 summary(ADI_mod)
 
 grid <- modelEclipse_df |> ungroup() |>
   data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-            eclipse = modelEclipse_df |> pull(eclipse) |> levels()
+            day_factor = modelEclipse_df |> pull(day_factor) |> levels()
   )
 
 gam_aug <- broom::augment(ADI_mod, newdata = grid)
 
 ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelEclipse_df, aes(y = fullADI, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
+  geom_point(data = modelEclipse_df, aes(y = fullADI, colour = day_factor), alpha = 0.1) +
+  geom_line(aes(colour = day_factor),
             linewidth = 2) +
   scale_colour_viridis_d() +
   theme_minimal() +
   labs(colour = "eclipse_or_not",
-       y = "aei")
-
-# Looking at time before
-ADI_mod2<- mgcv::gam(fullADI~ s(hour_numeric, by = eclipse)+
-                       eclipse
-                     , data = modelBeforeEclipse_df)
-summary(ADI_mod2)
-
-grid <- modelBeforeEclipse_df |> ungroup() |>
-  data_grid(hour_numeric = seq_range(hour_numeric, n = 40),
-            eclipse = modelBeforeEclipse_df |> pull(eclipse) |> levels()
-  )
-
-gam_aug <- broom::augment(ADI_mod2, newdata = grid)
-
-ggplot(data = gam_aug, aes(x = hour_numeric, y = .fitted)) +
-  geom_point(data = modelBeforeEclipse_df, aes(y = fullADI, colour = eclipse), alpha = 0.1) +
-  geom_line(aes(colour = factor(eclipse)),
-            linewidth = 2) +
-  scale_colour_viridis_d() +
-  theme_minimal() +
-  labs(colour = "eclipse_or_not",
-       y = "aei")
-
+       y = "aei")+
+  scale_x_continuous(name = "Time", breaks = c(eclipse_start, hms(00,00,15), hms(00,00,16), eclipse_end))
 
 
 
